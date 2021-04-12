c1a1afbba74402e09a7699f7607d001dd2c2d8a5
ajarombek <ajarombek95@gmail.com>
2021-04-11 22:09:19 -0400

    jss demo implemented

--- 2021/04-Apr/04-25-jss/index.js
+++ 2021/04-Apr/04-25-jss/index.js
@@ -9,6 +9,12 @@ import preset from "jss-preset-default";
 
 jss.setup(preset());
 
+const robotoSlabMixin = {
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontFamily: 'RobotoSlab-Regular, Helvetica, serif'
+}
+
 const styles = {
     '@global': {
         body: {
@@ -19,16 +25,90 @@ const styles = {
     page: {
         width: '100%',
         height: '100vh',
-        minHeight: '500px',
+        minHeight: 500,
         backgroundColor: '#222',
-        margin: 0
-    }
+        margin: 0,
+        paddingTop: 25
+    },
+    exerciseLog: {
+        width: '70%',
+        backgroundColor: '#99D699',
+        border: '2px solid rgb(136, 136, 136)',
+        margin: '0 auto',
+        padding: 7,
+        boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.2)',
+        borderRadius: 3
+    },
+    headerSection: {
+        display: 'flex'
+    },
+    titles: {
+        display: 'block'
+    },
+    titleLink: {
+        color: 'rgb(51, 51, 51)',
+        fontSize: 16,
+        textDecoration: 'none',
+        ...robotoSlabMixin
+    },
+    title: {
+        fontSize: 16,
+        margin: 0,
+        textDecoration: 'underline',
+        ...robotoSlabMixin
+    },
+    metadata: {
+        display: 'block',
+        margin: '0 0 0 auto',
+
+        '& > p': {
+            margin: 0,
+            fontSize: 14,
+            textAlign: 'right',
+            ...robotoSlabMixin
+        }
+    },
+    bodySection: {
+        marginTop: 20
+    },
+    dataField: {
+        margin: 0,
+        fontSize: 14,
+        ...robotoSlabMixin
+    },
+    description: {
+        marginTop: 10,
+        fontSize: 14,
+        ...robotoSlabMixin
+    },
 };
 
 const { classes } = jss.createStyleSheet(styles).attach();
 
 document.body.innerHTML = `
     <div class="${classes.page}">
-    
+        <div class="${classes.exerciseLog}">
+            <div class="${classes.headerSection}">
+                <div class="${classes.titles}">
+                    <a class="${classes.titleLink}">Andrew Jarombek</a>
+                    <h6 class="${classes.title}">Long Run</h6>
+                </div>
+                <div class="${classes.metadata}">
+                    <p>Apr. 11th, 2021</p>
+                    <p>RUN</p>
+                </div>
+            </div>
+            <div class="${classes.bodySection}">
+                <div>
+                    <p class="${classes.dataField}">Location: Riverside, CT</p>
+                    <p class="${classes.dataField}">14.01 miles</p>
+                    <p class="${classes.dataField}">1:39:03 (7:04/mi)</p>
+                </div>
+                <div class="${classes.description}">
+                    Went home to CT for the weekend to use the track on Saturday.  Got a nice long run in this morning 
+                    as well, longest since last summer.
+                </div>
+            </div>
+        </div>
     </div>
 `;
